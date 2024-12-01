0a4a814b6ab9c4b08520c64cc69a0f09a1042282
yangjunsik <wnstlr1379@naver.com>
2024-12-01 20:24:12 +0900

    ui마무리 및 네비게이터와 경로설정으로 버튼 누르면 이동 가능하게 만듬

--- app/src/main/java/com/example/mobile_pj/navigation/NavGraph.kt
+++ app/src/main/java/com/example/mobile_pj/navigation/NavGraph.kt
@@ -7,7 +7,9 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.mobile_pj.ui.screens.DashboardScreen
 import com.example.mobile_pj.ui.screens.LoginScreen
+import com.example.mobile_pj.ui.screens.QAPage
 import com.example.mobile_pj.ui.screens.SignUpScreen
+import com.example.mobile_pj.ui.screens.StatisticsPage
 
 @Composable
 fun NavGraph(
@@ -15,21 +17,30 @@ fun NavGraph(
 ) {
     NavHost(
         navController = navController,
-        startDestination = "login"
+        startDestination = Routes.LOGIN // Routes를 사용
     ) {
-        composable("login") {
+        composable(Routes.LOGIN) { // 로그인 화면
             LoginScreen(
-                onSignUpClick = { navController.navigate("signup") },
-                onLoginClick = { navController.navigate("dashboard") }
+                onSignUpClick = { navController.navigate(Routes.SIGNUP) },
+                onLoginClick = { navController.navigate(Routes.DASHBOARD) }
             )
         }
-        composable("signup") {
+        composable(Routes.SIGNUP) { // 회원가입 화면
             SignUpScreen(
-                onRegisterClick = { navController.navigate("login") }
+                onRegisterClick = { navController.popBackStack(Routes.LOGIN, false) }
             )
         }
-        composable("dashboard") {
-            DashboardScreen()
+        composable(Routes.DASHBOARD) { // 대시보드 화면
+            DashboardScreen(
+                onQAClick = { navController.navigate(Routes.QA) },
+                onStatisticsClick = { navController.navigate(Routes.STATISTICS) }
+            )
+        }
+        composable(Routes.QA) {
+            QAPage()
+        }
+        composable(Routes.STATISTICS) {
+            StatisticsPage()
         }
     }
 }
